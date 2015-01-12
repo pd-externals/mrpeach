cb154c5b3d739058555cbaafb13388c1917da52e
Martin Peach <chakekatzil@gmail.com>
2015-01-12 16:12:32 +0000

    Correct out of range format messages.
    git-svn-id: https://svn.code.sf.net/p/pure-data/svn/trunk/externals/mrpeach@17406 df7eb9d6-776f-4174-a42d-0ffd8d5d5b5d

--- slipdec/slipdec.c
+++ slipdec/slipdec.c
@@ -105,9 +105,9 @@ static void slipdec_list(t_slipdec *x, t_symbol *s, int ac, t_atom *av)
         if (c != f)
         {
             /* abort, input list needs to be fixed before this is gonna wuk */
-            pd_error (x, "slipdec: input %d out of range [0..255]", f);
-			x->x_valid_SLIP = 0; /* not valid SLIP */
-	        slipdec_dump(x,0);// reset
+            pd_error (x, "slipdec: input %f out of range [0..255]", f);
+			      x->x_valid_SLIP = 0; /* not valid SLIP */
+	          slipdec_dump(x,0);// reset
             return;
         }
         if(SLIP_END == c)
@@ -159,9 +159,9 @@ static void slipdec_float(t_slipdec *x, t_float f)
     if (c != f)
     {
         /* abort, input list needs to be fixed before this is gonna wuk */
-        pd_error (x, "slipdec: input %d out of range [0..255]", f);
-		x->x_valid_SLIP = 0; /* not valid SLIP */
-		slipdec_dump(x,0);/* reset */
+        pd_error (x, "slipdec: input %f out of range [0..255]", f);
+    		x->x_valid_SLIP = 0; /* not valid SLIP */
+    		slipdec_dump(x,0);/* reset */
         return;
     }
     if(SLIP_END == c)
